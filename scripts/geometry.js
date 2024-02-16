088a21682d6ce350696c479d9d1c0d65c57d7066
Koushik <koushikdas798@gmail.com>
2024-02-16 12:43:10 +0600

    Pentagon Section Completed

--- scripts/geometry.js
+++ scripts/geometry.js
@@ -27,3 +27,30 @@ function calculateRectangleArea() {
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = rectangleArea;
 }
+
+
+function calculateParallelogramArea() {
+    const base = getInputValueById('parallelogram-base');
+    const height = getInputValueById('parallelogram-height');
+    const area = base * height;
+    setInnerTextById('parallelogram-area', area); 
+}
+
+function calculatePentagonArea() {
+    const perimeter = getInputValueById('pentagon-perimeter');
+    const apothem = getInputValueById('pentagon-apothem');
+    const area = 0.5 * perimeter * apothem;
+    setInnerTextById('pentagon-area', area);
+}
+
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
